af17c70f95a44ad652a8ada48e9875b5e77a4e6b
独醒侠 <270307872@qq.com>
2020-09-30 18:19:57 +0800

    优化配置 1、升级pom依赖版本。 2、屏蔽打包配置内容。 3、提交权限认证异常类

---
+++ common-core/src/main/java/cn/dyaoming/errors/AppAccessException.java
@@ -0,0 +1,99 @@
+package cn.dyaoming.errors;
+
+/**
+ * <p>
+ * 系统权限异常
+ * </p>
+ *
+ * @author DYAOMING
+ * @version 0.0.5
+ * @since 2020-09-14
+ */
+public class AppAccessException extends AppRunTimeException {
+
+	private static final long serialVersionUID = -1522975184308076926L;
+
+	/**
+	 * <p>
+	 * 构造函数
+	 * </p>
+	 *
+	 */
+	public AppAccessException() {
+		super("Unauthorized access");
+	}
+
+	/**
+	 * <p>
+	 * 构造函数
+	 * </p>
+	 *
+	 * @param message String类型 异常信息
+	 */
+	public AppAccessException(String message) {
+		super(message);
+	}
+
+	/**
+	 * <p>
+	 * 构造函数
+	 * </p>
+	 *
+	 * @param code    String类型 异常标识
+	 * @param message String类型 异常信息
+	 */
+	public AppAccessException(String code, String message) {
+		super(message);
+		setCode(code);
+	}
+
+	/**
+	 * <p>
+	 * 构造函数
+	 * </p>
+	 *
+	 * @param message String类型 异常信息
+	 * @param cause   异常
+	 */
+	public AppAccessException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	/**
+	 * <p>
+	 * 构造函数
+	 * </p>
+	 *
+	 * @param code    String类型 异常标识
+	 * @param message String类型 异常信息
+	 * @param cause   Throwable类型 异常
+	 */
+	public AppAccessException(String code, String message, Throwable cause) {
+		super(message, cause);
+		setCode(code);
+	}
+
+	/**
+	 * <p>
+	 * 构造函数
+	 * </p>
+	 *
+	 * @param cause String类型 异常
+	 */
+	public AppAccessException(Throwable cause) {
+		super(cause);
+	}
+
+    public static AppAccessException create(String message) {
+        return new AppAccessException(message);
+    }
+
+    public static AppAccessException create(String code, String message) {
+        return new AppAccessException(code, message);
+    }
+
+    public static AppAccessException create(String code, String message, Throwable cause) {
+        return new AppAccessException(code, message, cause);
+    }
+
+}
