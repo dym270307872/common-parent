bae4444899e1a6f34fbbac5645ad8c891ab7e6c7
独醒侠 <270307872@qq.com>
2021-03-01 18:40:54 +0800

    合并修改的代码

--- common-util/src/main/java/cn/dyaoming/utils/HashUtil.java
+++ common-util/src/main/java/cn/dyaoming/utils/HashUtil.java
@@ -1,11 +1,9 @@
 package cn.dyaoming.utils;
 
-
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 
 import cn.dyaoming.errors.AppUtilException;
-
 
 /**
  * <p>
@@ -18,60 +16,58 @@ import cn.dyaoming.errors.AppUtilException;
  */
 public class HashUtil {
 
-    private static final String MD5 = "MD5";
-    private static final String SHA1 = "SHA-1";
-    private static final String SHA256 = "SHA-256";
+	private static final String HASH_MD5 = "MD5";
+	private static final String HASH_SHA_1 = "SHA-1";
+	private static final String HASH_SHA_256 = "SHA-256";
 
-    public static String md5(String value) {
-        try {
+	public static String md5(String value) {
+		try {
+			byte[] bytes = encode(value.getBytes(StandardCharsets.UTF_8), HASH_MD5);
+			return BaseCodingUtil.toHexString(bytes);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
-            return BaseCodingUtil.toHexString(encode(value.getBytes(StandardCharsets.UTF_8), MD5));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+	public static String sha1(String value) {
+		try {
+			byte[] bytes = encode(value.getBytes(StandardCharsets.UTF_8), HASH_SHA_1);
+			return BaseCodingUtil.toHexString(bytes);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
+	public static String sha256(String value) {
+		try {
+			byte[] bytes = encode(value.getBytes(StandardCharsets.UTF_8), HASH_SHA_256);
+			return BaseCodingUtil.toHexString(bytes);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
-
-    public static String sha1(String value) {
-        try {
-            return BaseCodingUtil.toHexString(encode(value.getBytes(StandardCharsets.UTF_8), SHA1));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-
-
-    public static String sha256(String value) {
-        try {
-            return BaseCodingUtil.toHexString(encode(value.getBytes(StandardCharsets.UTF_8), SHA256));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-
-
-    /**
-     * <p>hash编码工具类</p>
-     * 
-     * @param bytes 需要编码的数组
-     * @param encodeType 编码类型
-     * @return 编码结果
-     * @throws AppUtilException 工具类异常
-     */
-    public static byte[] encode(byte[] bytes, String encodeType) throws AppUtilException {
-        try {
-            MessageDigest messageDigest = MessageDigest.getInstance(encodeType);
-            messageDigest.update(bytes);
-            return messageDigest.digest();
-        } catch (Exception e) {
-            throw AppUtilException.create("hash编码异常，原因是：" + e.getMessage(), e);
-        }
-    }
+	/**
+	 * <p>
+	 * hash编码工具类
+	 * </p>
+	 * 
+	 * @param bytes      需要编码的数组
+	 * @param encodeType 编码类型
+	 * @return 编码结果
+	 * @throws AppUtilException 工具类异常
+	 */
+	public static byte[] encode(byte[] bytes, String encodeType) throws AppUtilException {
+		try {
+			MessageDigest messageDigest = MessageDigest.getInstance(encodeType);
+			messageDigest.update(bytes);
+			return messageDigest.digest();
+		} catch (Exception e) {
+			throw AppUtilException.create("hash编码异常，原因是：" + e.getMessage(), e);
+		}
+	}
 
 }
--- common-util/src/main/java/cn/dyaoming/utils/HmacUtil.java
+++ common-util/src/main/java/cn/dyaoming/utils/HmacUtil.java
@@ -9,6 +9,8 @@ import javax.crypto.spec.SecretKeySpec;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import cn.dyaoming.errors.AppUtilException;
+
 /**
  * <p>
  * hmac加密工具类
@@ -20,6 +22,7 @@ import org.slf4j.LoggerFactory;
  */
 public class HmacUtil {
 	private final static Logger LOGGER = LoggerFactory.getLogger(HmacUtil.class);
+	
 	private static final String HMAC_MD5 = "HmacMD5";
 	private static final String HMAC_SHA_1 = "HmacSHA1";
 	private static final String HMAC_SHA_256 = "HmacSHA256";
@@ -48,10 +51,10 @@ public class HmacUtil {
 		return outPut;
 	}
 
-	public static String sha256(String message, String key) {
+	public static String sha256(String message, String encryptKey) {
 		String outPut = null;
 		try {
-			byte[] bytes = encode(message.getBytes(StandardCharsets.UTF_8), key.getBytes(StandardCharsets.UTF_8),
+			byte[] bytes = encode(message.getBytes(StandardCharsets.UTF_8), encryptKey.getBytes(StandardCharsets.UTF_8),
 					HMAC_SHA_256);
 			outPut = BaseCodingUtil.toHexString(bytes);
 		} catch (Exception e) {
@@ -60,16 +63,23 @@ public class HmacUtil {
 		return outPut;
 	}
 
-	public static byte[] encode(byte[] bytes, byte[] key, String encodeType) {
+	/**
+	 * <p>hmac编码方法</p>
+	 *
+	 * @param messageBytes 加密文本字节组
+	 * @param encryptKeyBytes
+	 * @param encodeType
+	 * @return
+	 * @throws AppUtilException
+	 */
+	public static byte[] encode(byte[] messageBytes, byte[] encryptKeyBytes, String encodeType) throws AppUtilException {
 		try {
 			Mac hmacMac = Mac.getInstance(encodeType);
-			SecretKeySpec secretKey = new SecretKeySpec(key, encodeType);
+			SecretKeySpec secretKey = new SecretKeySpec(encryptKeyBytes, encodeType);
 			hmacMac.init(secretKey);
-			return hmacMac.doFinal(bytes);
+			return hmacMac.doFinal(messageBytes);
 		} catch (Exception e) {
-			e.printStackTrace();
+			throw AppUtilException.create("","",e);
 		}
-
-		return new byte[0];
 	}
 }
