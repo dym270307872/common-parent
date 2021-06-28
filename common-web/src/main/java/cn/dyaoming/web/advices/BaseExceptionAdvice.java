eafb81ba4ddde03026acc80678a4c6e8364bd275
独醒侠 <270307872@qq.com>
2021-06-28 11:45:16 +0800

    修改版本号 加工请求方式不正确为404错误码

--- common-web/src/main/java/cn/dyaoming/web/advices/BaseExceptionAdvice.java
+++ common-web/src/main/java/cn/dyaoming/web/advices/BaseExceptionAdvice.java
@@ -1,9 +1,12 @@
 package cn.dyaoming.web.advices;
 
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,9 +15,10 @@ import cn.dyaoming.errors.BaseException;
 import cn.dyaoming.errors.BaseRunTimeException;
 import cn.dyaoming.models.BaseRestModel;
 
-
 /**
- * <p>异常统一拦截过滤器</p>
+ * <p>
+ * 异常统一拦截过滤器
+ * </p>
  * 
  * @author DYAOMING
  * @since 2020-12-22
@@ -24,54 +28,71 @@ import cn.dyaoming.models.BaseRestModel;
 @ControllerAdvice
 public class BaseExceptionAdvice {
 
-    protected Logger log = LoggerFactory.getLogger(this.getClass());
+	protected Logger log = LoggerFactory.getLogger(this.getClass());
 
+	/**
+	 * <p>
+	 * exception异常统一拦截器
+	 * </p>
+	 * 
+	 * @param exception 异常对象
+	 * @return rest统一对象
+	 */
+	@ExceptionHandler(value = Exception.class)
+	@Order(500000)
+	@ResponseBody
+	public BaseRestModel handleException(Exception exception) {
+		log.error("系统发生未察觉异常，异常内容：" + exception.getMessage(), exception);
+		return BaseRestModel.failed();
+	}
 
+	/**
+	 * <p>
+	 * 自定义baseException异常统一拦截器
+	 * </p>
+	 * 
+	 * @param baseException 异常对象
+	 * @return rest统一对象
+	 */
+	@ExceptionHandler(value = BaseException.class)
+	@Order(100000)
+	@ResponseBody
+	public BaseRestModel handleAppBaseException(BaseException baseException) {
+		log.error("系统发生异常，通过控制层抛向前端。", baseException);
+		return BaseRestModel.failed(baseException.getCode(), baseException.getMessage());
+	}
 
-    /**
-     * <p>exception异常统一拦截器</p>
-     * 
-     * @param exception 异常对象
-     * @return rest统一对象
-     */
-    @ExceptionHandler(value = Exception.class)
-    @Order(500000)
-    @ResponseBody
-    public BaseRestModel handleException(Exception exception) {
-        log.error("系统发生未察觉异常，异常内容：" + exception.getMessage(), exception);
-        return BaseRestModel.failed();
-    }
+	/**
+	 * <p>
+	 * 自定义运行时异常统一拦截器
+	 * </p>
+	 * 
+	 * @param runTimeException 异常对象
+	 * @return rest统一对象
+	 */
+	@ExceptionHandler(value = BaseRunTimeException.class)
+	@Order(80000)
+	@ResponseBody
+	public BaseRestModel handleAppRunTimeException(BaseRunTimeException runTimeException) {
+		log.debug("系统发生运行时异常，异常原因：" + runTimeException.getMessage(), runTimeException);
+		return BaseRestModel.failed(runTimeException.getCode(), runTimeException.getMessage());
+	}
 
-
-
-    /**
-     * <p>自定义baseException异常统一拦截器</p>
-     * 
-     * @param baseException 异常对象
-     * @return rest统一对象
-     */
-    @ExceptionHandler(value = BaseException.class)
-    @Order(100000)
-    @ResponseBody
-    public BaseRestModel handleAppBaseException(BaseException baseException) {
-        log.error("系统发生异常，通过控制层抛向前端。", baseException);
-        return BaseRestModel.failed(baseException.getCode(), baseException.getMessage());
-    }
-
-
-
-    /**
-     * <p>自定义运行时异常统一拦截器</p>
-     * 
-     * @param runTimeException 异常对象
-     * @return rest统一对象
-     */
-    @ExceptionHandler(value = BaseRunTimeException.class)
-    @Order(80000)
-    @ResponseBody
-    public BaseRestModel handleAppRunTimeException(BaseRunTimeException runTimeException) {
-        log.debug("系统发生运行时异常，异常原因：" + runTimeException.getMessage(), runTimeException);
-        return BaseRestModel.failed(runTimeException.getCode(), runTimeException.getMessage());
-    }
-
+	/**
+	 * <p>
+	 * exception异常统一拦截器
+	 * </p>
+	 * 
+	 * @param exception 异常对象
+	 * @return rest统一对象
+	 */
+	@ExceptionHandler(value = HttpRequestMethodNotSupportedException.class)
+	@Order(10000)
+	public String handleNotSupportedException(HttpRequestMethodNotSupportedException exception,
+			HttpServletRequest httpRequest) {
+		log.debug("系统发生异常，异常内容：请求路径：" + httpRequest.getRequestURI(), exception);
+        //设置状态码
+        httpRequest.setAttribute("javax.servlet.error.status_code", HttpStatus.NOT_FOUND.value());
+        return "forward:/error";
+	}
 }
